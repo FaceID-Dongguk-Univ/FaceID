19691c045022d30a024a62086f51c9ca0480c15d
good-riverdeer <67779236+good-riverdeer@users.noreply.github.com>
2021-03-30 17:50:50 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+import cv2
+import imutils
+from imutils.video import VideoStream
+import dlib
+import time
+
+print("[INFO] loading model weights...")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("weights/shape_predictor_68_face_landmarks.dat")
+
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2)
+
+while True:
+    frame = vs.read()
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_detector = detector(frame_gray, 1)
+    print("The number of faces detected : {}".format(len(face_detector)))
+
+    for face in face_detector:
+        landmarks = predictor(frame, face)
+
+        landmark_list = []
+        for p in landmarks.parts():
+            landmark_list.append([p.x, p.y])
+
+        h = landmark_list[8][1] - landmark_list[27][1]
+        w = landmark_list[16][0] - landmark_list[0][0]
+        (x, y) = landmark_list[33]
+
+        # if 얼굴 움직임 테스트를 만족하면:
+        #     label = "Verified"
+        #     color = (255, 0, 0)
+        #     print("[INFO] you're not picture!")
+        #     얼굴 크롭해서 얼굴 인식 모델에 넘겨줌
+        # else:
+        #     label = "Robot"
+        #     color = (0, 0, 255)
+
+        cv2.rectangle(frame, (x - w, y + h), (x + w, y - 2 * h),
+                      (0, 0, 255), 3)
+        # cv2.rectangle(frame, (x - w, y + h), (x + w, y - 2 * h),
+        #               color, 3)
+        # cv2.putText(frame, label, (x - w, y + h - 10),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
