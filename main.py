ce11d273e16fcb9ca7f441428b7ac529d0a90379
good-riverdeer <rornf6232@gmail.com>
2021-04-13 01:01:56 +0900

    update main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import numpy as np
+import tensorflow as tf
 import cv2
-# import imutils
-from imutils.video import VideoStream
 import dlib
 import time
-# import argparse
-from idcard import is_verified_idnum, is_verified_age
-import tensorflow as tf
+from imutils import face_utils
+from imutils.video import VideoStream
+
+from networks.recognition.resnet50_arcface import resnet50_arcface
+from utils import is_verified_idnum, is_verified_age, crop_face_from_id, eye_aspect_ratio, mouth_aspect_ratio
 
 # ap = argparse.ArgumentParser()
 # ap.add_argument("-i", "--idfile", type=str,
@@ -13,21 +15,48 @@ import tensorflow as tf
 # args = vars(ap.parse_args())
 
 
-def identification(detector, predictor, fr_model):
+def main(detector, predictor):
+
+    # --- human test parameters ---
+    (l_start, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (r_start, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    (mouth_start, mouth_end) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+    eye_counter = 0
+    mouth_counter = 0
+    smile_counter = 0
+    # is_eyes_closed = False
+    # is_mouth_open = False
+    # is_smiling = False
+    is_human = False
+    eye_ar_thresh = 0.3
+    mouth_ar_thresh = 0.6
+    smile_ar_thresh = 0.2
+    frame_ar_thresh = 20
+    task = np.random.randint(1, 4)
+
     print("[INFO] starting video stream...")
     vs = VideoStream(src=0).start()
     time.sleep(2)
 
+    if task == 1:
+        print("[INFO] *** Please close your eyes ***")
+    elif task == 2:
+        print("[INFO] *** Please open your mouth ***")
+    else:
+        print("[INFO] *** Please smile for me ***")
+    print("*** If you passed \"Human Test\", press \'s\' button and take a picture ***")
+
     while True:
         frame = vs.read()
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
         face_detector = detector(frame_gray, 1)
-        # print("[INFO] The number of faces detected : {}".format(len(face_detector)))
+
+        cropped = frame.copy()
 
         for face in face_detector:
             landmarks = predictor(frame, face)
 
+            # --- draw 68 face landmarks ---
             landmark_list = []
             for p in landmarks.parts():
                 landmark_list.append([p.x, p.y])
@@ -36,45 +65,92 @@ def identification(detector, predictor, fr_model):
             h = landmark_list[8][1] - landmark_list[27][1]
             w = landmark_list[16][0] - landmark_list[0][0]
             (x, y) = landmark_list[33]
+            cv2.rectangle(frame, (x-w, y+h), (x+w, y-h), (0, 0, 255), 3)
 
-            # if 얼굴 움직임 테스트를 만족하면:
-            #     label = "Verified"
-            #     color = (255, 0, 0)
-            #     print("[INFO] you're not picture!")
-            #     얼굴 크롭해서 얼굴 인식 모델에 넘겨줌
-            #     break
-            # else:
-            #     label = "Robot"
-            #     color = (0, 0, 255)
-            cropped = frame.copy()
-            cropped = cropped[y-2*h: y+h, x-w: x+w]
-            print(cropped.shape)
-            # print()
+            # cropped = cropped.copy()
+            cropped = cropped[y - h: y + h, x - w: x + w]
+            # print(cropped.shape)
 
+            # --- human test ---
+            landmarks = face_utils.shape_to_np(landmarks)
 
-            cv2.rectangle(frame, (x - w, y + h), (x + w, y - 2 * h),
-                          (0, 0, 255), 3)
-            # cv2.rectangle(frame, (x - w, y + h), (x + w, y - 2 * h),
-            #               color, 3)
-            # cv2.putText(frame, label, (x - w, y + h - 10),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            if task == 1:
+                left_eye = landmarks[l_start: l_end]
+                right_eye = landmarks[r_start: r_end]
+                left_ear = eye_aspect_ratio(left_eye)
+                right_ear = eye_aspect_ratio(right_eye)
+                ear = (left_ear + right_ear) / 2.0
+                # print("ear: ", ear)
+
+                if ear < eye_ar_thresh:
+                    eye_counter += 1
+                    cv2.putText(frame, "EYES are closed", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                    if eye_counter >= frame_ar_thresh:
+                        is_human = True
+                else:
+                    eye_counter = 0
+                    # is_human = False
+
+            elif task == 2:
+                in_mouth = landmarks[mouth_start: mouth_end]
+                mar = mouth_aspect_ratio(in_mouth)
+                # print("mar: ", mar)
+
+                if mar > mouth_ar_thresh:
+                    mouth_counter += 1
+                    cv2.putText(frame, "MOUTH is open", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+                    if mouth_counter >= frame_ar_thresh:
+                        is_human = True
+                else:
+                    mouth_counter = 0
+                    # is_human = False
+
+            else:
+                in_mouth = landmarks[mouth_start: mouth_end]
+                mar = mouth_aspect_ratio(in_mouth)
+                # print("mar: ", mar)
+
+                if mar < smile_ar_thresh:
+                    smile_counter += 1
+                    cv2.putText(frame, "You're SMILING", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    if smile_counter >= frame_ar_thresh:
+                        is_human = True
+                else:
+                    smile_counter = 0
+                    # is_human = False
+
+            if is_human:
+                cv2.putText(frame, "You're Human, thank you", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+            # is_human = is_eyes_closed or is_mouth_open or is_smiling
+            # print(is_human)
 
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1)
 
-        if key == ord("q"):
+        if (key == ord("s") and is_human) or key == ord('q'):
             break
+
+    # --- face recognition ---
+    # if is_human:
+    #     is_valid = recognition_model(cropped, sr_id_cropped)
 
     cv2.destroyAllWindows()
     vs.stop()
 
 
 if __name__ == "__main__":
+    LR_SHAPE = (100 // 4, 100 // 4, 3)
+    INPUT_SHAPE = (100, 100, 3)
+    N_CLASSES = 5749
+
     print("[INFO] loading face detector weights...")
     main_detector = dlib.get_frontal_face_detector()
     main_predictor = dlib.shape_predictor("weights/shape_predictor_68_face_landmarks.dat")
-    arcface = tf.keras.models.load_model("./weights/resnet50_arcface_epochs50.hdf5")
-    arcface = tf.keras.Model(inputs=arcface.input, outputs=arcface.layers[-3].output)
+    # arcface = resnet50_arcface(INPUT_SHAPE, N_CLASSES)
+    # arcface = tf.keras.models.load_model("./weights/resnet50_arcface_epochs50.hdf5")
+    # arcface = tf.keras.Model(inputs=arcface.input, outputs=arcface.layers[-3].output)
+    # resolution_model =
 
     while True:
         file_name = input("[INFO] Type ID card image file path: ")
@@ -83,12 +159,16 @@ if __name__ == "__main__":
             break
 
         print("[INFO] Verifying ID card...")
-        idcard_image = cv2.imread(file_name)
+        id_image = cv2.imread(file_name)
 
-        if is_verified_idnum(idcard_image) and is_verified_age(idcard_image):
+        if is_verified_idnum(id_image) and is_verified_age(id_image):
+            # sr_id_cropped = crop_face_from_id(id_image)
+            # sr_id_cropped = cv2.resize(sr_id_cropped, LR_SHAPE)
+            # sr_id_cropped = resolution_model(sr_id_cropped)
+
             print("[INFO] Your ID card is verified")
-            identification(main_detector, main_predictor, arcface)
+            main(main_detector, main_predictor)
 
         else:
-            print("[ERROR] Invalid ID card!")
+            print("[INFO] Invalid ID card!")
 
--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import re
 import time
-
-import pytesseract as tess
 import dlib
 import cv2
 import numpy as np
+import pytesseract as tess
+
+from scipy.spatial import distance
 
 # ALL = list(range(0, 68))
 # RIGHT_EYEBROW = list(range(17, 22))
@@ -17,7 +18,7 @@ import numpy as np
 # JAWLINE = list(range(0, 17))
 
 
-def crop_face_idcard(cv_image):
+def crop_face_from_id(cv_image):
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor('weights/shape_predictor_68_face_landmarks.dat')
 
@@ -29,31 +30,23 @@ def crop_face_idcard(cv_image):
     if len(face_detector) == 1:
         face = face_detector[0]
 
-        # cv2.rectangle(image, (face.left(), face.top()), (face.right(), face.bottom()),
-        #               (0, 0, 255), 3)
-
         landmarks = predictor(cv_image, face)
 
         landmark_list = []
-
         for p in landmarks.parts():
             landmark_list.append([p.x, p.y])
             # cv2.circle(image, (p.x, p.y), 2, (0, 255, 0), -1)
-
         h = landmark_list[8][1] - landmark_list[27][1]
         w = landmark_list[16][0] - landmark_list[0][0]
         (x, y) = landmark_list[33]
 
-        # cv2.rectangle(image,  (x-w, y+h), (x+w, y-2*h),
-        #               (255, 0, 0), 3)
-
         cropped = cv_image.copy()
-        cropped = cropped[y-2*h: y+h, x-w: x+w]
+        cropped = cropped[y - h: y + h, x - w: x + w]
 
         return cropped
 
     else:
-        print("invalid")
+        raise RuntimeError("[Error] Cannot find ID card")
 
 
 def get_idnum(cv_image):
@@ -66,41 +59,66 @@ def get_idnum(cv_image):
             
         return text
     
-    except Exception as e:
-        print("[INFO] Cannot find ID card")
+    except RuntimeError:
+        print("[Error] Cannot find ID card")
 
 
 def is_verified_idnum(image):
-    id_num = get_idnum(image)
+    try:
+        id_num = get_idnum(image)
 
-    weights = np.array([2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5])
-    id_num_array = np.array(list(id_num[:-1]), np.int32)
+        weights = np.array([2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5])
+        id_num_array = np.array(list(id_num[:-1]), np.int32)
 
-    if int(id_num[-1]) == (11 - np.dot(weights, id_num_array.transpose()) % 11) % 11:
-        return True
-    
-    else:
-        return False
+        if int(id_num[-1]) == (11 - np.dot(weights, id_num_array.transpose()) % 11) % 11:
+            return True
+
+        else:
+            return False
+
+    except RuntimeError:
+        print("[Error] Cannot find ID card")
 
 
 def is_verified_age(image):
-    id_num = get_idnum(image)
-    year_curr = int(time.strftime('%Y', time.localtime(time.time())))
-    if id_num[6] == '1' or id_num[6] == '2':
-        year_birth = int(id_num[:1]) + 1900
-    else:
-        year_birth = int(id_num[:1]) + 2000
+    try:
+        id_num = get_idnum(image)
+        year_curr = int(time.strftime('%Y', time.localtime(time.time())))
+        if id_num[6] == '1' or id_num[6] == '2':
+            year_birth = int(id_num[:1]) + 1900
+        else:
+            year_birth = int(id_num[:1]) + 2000
 
-    age = year_curr - year_birth
-    if age >= 19:
-        return True
-    else:
-        return False
+        age = year_curr - year_birth
+        if age >= 19:
+            return True
+        else:
+            return False
+
+    except RuntimeError:
+        print("[Error] Cannot find ID card")
+
+
+def eye_aspect_ratio(eye):
+    a = distance.euclidean(eye[1], eye[5])
+    b = distance.euclidean(eye[2], eye[4])
+    c = distance.euclidean(eye[0], eye[3])
+    ear = (a + b) / (2.0 * c)
+    return ear
+
+
+def mouth_aspect_ratio(mouth):
+    a = distance.euclidean(mouth[14], mouth[18]) # 51, 59
+    b = distance.euclidean(mouth[4], mouth[8]) # 53, 57
+    c = distance.euclidean(mouth[12], mouth[16]) # 49, 55
+    mar = (a + b) / (2.0 * c)
+    return mar
 
 
 if __name__ == "__main__":
     # image = cv2.imread("your idcard here")
     image = cv2.imread("resource/idcard.JPG")
+    # image = cv2.imread("resource/license.jpg")
 
     # get ID number
     id_num = get_idnum(image)
@@ -113,6 +131,6 @@ if __name__ == "__main__":
     print(is_verified_age(image))
 
     # crop face from ID card
-    result = crop_face_idcard(image)
+    result = crop_face_from_id(image)
     cv2.imshow("result", result)
     cv2.waitKey(0)
